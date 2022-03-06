44c3d677f4a9913da7ebacfa60e433981b42baab
shazinii <84476509+shazinii@users.noreply.github.com>
2022-03-06 07:07:00 -0500

    Update handleBrowser.js

--- scripts/handleBrowser.js
+++ scripts/handleBrowser.js
@@ -1,14 +1,14 @@
-// window.onbeforeunload = function (e) {
-//     e = e || window.event;
+window.onbeforeunload = function (e) {
+    e = e || window.event;
 
-//     // For IE and Firefox prior to version 4
-//     if (e) {
-//       e.returnValue = "Any string";
-//     }
+    // For IE and Firefox prior to version 4
+    if (e) {
+      e.returnValue = "Any string";
+    }
 
-//     // For Safari
-//     return "Any string";
-//   };
+    // For Safari
+    return "Any string";
+  };
 
 //Setup Local Storage:
 
